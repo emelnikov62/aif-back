5028e13e645385612f802a7ad801de7fbd8edb22
Евгений Мельников <melnikovea@region.vtb.ru>
2025-11-17 17:13:24 +0300

    #fix

--- src/main/java/ru/aif/aifback/services/tg/TgUtils.java
+++ src/main/java/ru/aif/aifback/services/tg/TgUtils.java
@@ -22,7 +22,7 @@ public final class TgUtils {
      * @param bot bot
      */
     public static void sendPhoto(Long id, byte[] file, String caption, Keyboard keyboard, TelegramBot bot) {
-        log.info("{}", bot.execute(new SendPhoto(id, file).caption(caption).replyMarkup(keyboard)));
+        log.info("{}", bot.execute(new SendPhoto(id, file).caption(caption).replyMarkup(keyboard).parseMode(ParseMode.HTML)));
     }
 
     /**
